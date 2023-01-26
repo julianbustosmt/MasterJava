8be6b710aa193dd654d9dbbfeef2c5e918d13c8c
Julian Esteban Bustos <julianestebanbustos@gmail.com>
2023-01-26 15:55:26 -0500

    Master en Java Udemy Seccion2 - Variables

--- Seccion2-Variables/Variables/src/ConversionDeTipos.java
+++ Seccion2-Variables/Variables/src/ConversionDeTipos.java
@@ -3,6 +3,8 @@ public class ConversionDeTipos {
 
         String numeroStr = "50";
 
+        // Conversion de String a tipos Primitivos
+
         int numeroInt = Integer.parseInt(numeroStr);
         System.out.println("numeroInt = " + numeroInt);
 
@@ -13,6 +15,37 @@ public class ConversionDeTipos {
         String logicoStr = "FaLsE";
         boolean logicoBoolean = Boolean.parseBoolean(logicoStr);
         System.out.println("logicoBoolean = " + logicoBoolean);
+
+        // Conversion de tipo Primitivo a String
+
+        int otroNumeroInt = 100;
+        System.out.println("otroNumeroInt = " + otroNumeroInt);
+
+        String otroNumeroStr = Integer.toString(otroNumeroInt);
+        System.out.println("otroNumeroStr = " + otroNumeroStr);
+
+        otroNumeroStr = String.valueOf(otroNumeroInt + 10);
+        System.out.println("otroNumeroStr = " + otroNumeroStr);
+
+        double otroRealDouble = 1.2322;
+        String otroRealStr = Double.toString(otroRealDouble);
+        System.out.println("otroRealStr = " + otroRealStr);
+        
+        otroRealStr = String.valueOf(1.2345e-2f);
+        System.out.println("otroRealStr = " + otroRealStr);
+
+        //Conversion entre tipos primitivos
+
+        int i = 42768;
+        short s = (short)i; // Esta conversion está mal hecha, ya que el número que deseamos castear supera el límite del primitivo asignado. Por lo tanto el resultado será ambiguo
+        System.out.println("s = " + s);
+        long l = i;
+        System.out.println("l = " + l);
+        System.out.println(Short.MAX_VALUE);
+        char b = (char) i;
+        System.out.println("b = " + b);
+        float f = (float) i;
+        System.out.println("f = " + f);
 
 
 
